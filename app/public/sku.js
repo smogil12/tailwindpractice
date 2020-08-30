08d79b65373e0e77a627cf0ea35b5807d4291eba
Spencer Mogil <smogil12@gmail.com>
2020-08-29 23:42:42 -0400

    start js logic

--- app/public/sku.js
+++ app/public/sku.js
@@ -1,1 +1,23 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  console.log("test2");
+  var queryURL =
+    "https://static.www.turnto.com/sitedata/qM9izDb9x6YaGMxsite/AQ0001-002/d/exportjson/lhuniZNPTi1LiGZLtF6vsZHYgvHYauth";
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    var results = response.events;
+
+    for (var i = 0; i < results.length; i++) {
+      console.log(results);
+      var statdiv = $("<div>");
+
+      var rating = results[i].rating;
+
+      statdiv.prepend(rating);
+
+      $("#skustats").prepend(statdiv);
+      console.log(response.data);
+    }
+  });
+});
